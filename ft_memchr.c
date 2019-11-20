762c7d013ae43c6f6f71ee374eaacfcaa74883c3
Svyatoslav Krasin <skrasin@e1z2r4p9.42.us.org>
2019-11-20 11:32:32 -0800

    prev v of memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 11:08:12 by skrasin           #+#    #+#             */
-/*   Updated: 2019/11/20 11:28:22 by skrasin          ###   ########.fr       */
+/*   Updated: 2019/11/20 11:32:01 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*c_p;
-	const uintmax_t		*lw_p;
-	uintmax_t			lw_c;
-	const uintmax_t		r_1 = 0x0101010101010101;
-	uintmax_t			r_c;
+	register const unsigned char	*str;
+	register unsigned char			ch;
 
-	c_p = (const unsigned char *)s;
-	lw_p = (const uintmax_t *)c_p;
-	r_c = (unsigned char)c | ((unsigned char)c << 8) | ((unsigned char)c << 16)
-													| ((unsigned char)c << 24);
-	r_c |= r_c << 32;
-	while (n >= sizeof(uintmax_t))
-	{
-		lw_c = *lw_p++ ^ r_c;
-		if ((((lw_c - r_1) && ~lw_c) & (r_c << 7)) != 0)
-			break ;
-		n -= sizeof(uintmax_t);
-	}
-	c_p = (const unsigned char *)lw_p;
-	while (n-- > 0)
-		if (*c_p++ == c)
-			return ((void *)c_p);
+	str = (unsigned char *)s;
+	ch = c;
+	while (n--)
+		if (*str++ == ch)
+			return ((unsigned char *)--str);
 	return (NULL);
 }
