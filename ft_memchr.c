07eaf8640c375e4943bf93e837ae12c151446373
Svyatoslav Krasin <skrasin@e1z2r4p9.42.us.org>
2019-11-20 11:28:55 -0800

    tmp v of memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 11:08:12 by skrasin           #+#    #+#             */
-/*   Updated: 2019/11/20 11:25:00 by skrasin          ###   ########.fr       */
+/*   Updated: 2019/11/20 11:28:22 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	const uintmax_t		r_1 = 0x0101010101010101;
 	uintmax_t			r_c;
 
+	c_p = (const unsigned char *)s;
 	lw_p = (const uintmax_t *)c_p;
 	r_c = (unsigned char)c | ((unsigned char)c << 8) | ((unsigned char)c << 16)
 													| ((unsigned char)c << 24);
