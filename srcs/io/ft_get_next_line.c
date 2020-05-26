7d224f9920f4e15bf0f86621da6e6a854e23d8b2
Sviatoslav <svet@Sviatoslavs-MacBook-Pro.local>
2020-05-26 13:15:40 -0700

    upd gnl

--- srcs/io/ft_get_next_line.c
+++ srcs/io/ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 15:00:39 by skrasin           #+#    #+#             */
-/*   Updated: 2020/05/26 13:06:31 by svet             ###   ########.fr       */
+/*   Updated: 2020/05/26 13:15:32 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int						ft_get_next_line(const int fd, char **line)
 	if (fd < 0 || line == NULL || read(fd, 0, 0) == -1 ||
 											(node = ft_lstsearch(fd)) == NULL)
 		return (-1);
-	while ((nl = ft_strchr(node->content, '\n')) != NULL)
+	while ((nl = ft_strchr(node->content, '\n')) == NULL)
 	{
 		if ((len = read(fd, buf, BUFF_SIZE)) <= 0)
 			break ;
