0c9dbed4ba3b2019c0632e89d4206cdb292c65ed
Sviatoslav <svet@Sviatoslavs-MacBook-Pro.local>
2020-05-26 13:13:15 -0700

    upd gnl

--- srcs/str/ft_strext.c
+++ srcs/str/ft_strext.c
@@ -6,14 +6,13 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/26 13:07:19 by svet              #+#    #+#             */
-/*   Updated: 2020/05/26 13:11:17 by svet             ###   ########.fr       */
+/*   Updated: 2020/05/26 13:13:09 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_memory.h"
 #include "ft_string.h"
 #include <stdlib.h>
-
 
 char	*ft_strext(char **dst, char const *src)
 {
