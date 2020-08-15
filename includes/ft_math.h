31bde8dfbf0af58284d264a8ae040d8f55f053cb
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2020-08-14 19:09:52 -0700

    add func for enhanced work with list

--- includes/ft_math.h
+++ includes/ft_math.h
@@ -6,16 +6,18 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 15:47:15 by svet              #+#    #+#             */
-/*   Updated: 2020/08/11 19:32:52 by svet             ###   ########.fr       */
+/*   Updated: 2020/08/14 19:02:32 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATH_H
 # define FT_MATH_H
 # include <_types/_intmax_t.h>
+# include <_types/_uintmax_t.h>
 # include <sys/types.h>
 
 unsigned int	ft_num_of_digs(intmax_t n, int base);
+unsigned int	ft_unum_of_digs(uintmax_t n, int base);
 
 typedef struct	s_parts
 {
