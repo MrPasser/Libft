132840ccaa7ad984ce63b1e5f43e97f151fb4669
Svyatoslav Krasin <skrasin@e1z2r9p31.42.us.org>
2020-08-31 15:17:21 -0700

    improve bonus handeling

--- includes/ft_list_bonus.h
+++ includes/ft_list_bonus.h
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: skrasin <skrasin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 21:10:49 by svet              #+#    #+#             */
-/*   Updated: 2020/05/20 11:27:11 by svet             ###   ########.fr       */
+/*   Updated: 2020/08/31 14:47:53 by skrasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIST_BONUS_H
 # define FT_LIST_BONUS_H
 # include <sys/_types/_size_t.h>
+
+# ifndef LIBFT_BONUS
+# include "ft_list.h"
+# endif
+
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+
+# ifdef LIBFT_BONUS
 
 typedef struct		s_list
 {
@@ -21,14 +33,11 @@ typedef struct		s_list
 }					t_list;
 
 t_list				*ft_lstnew(void const *content);
-void				ft_lstadd_front(t_list **lst, t_list *new);
-t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_list **lst, t_list *new);
-void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(void *),
 														void (*del)(void *));
-
 void				ft_lstrev(t_list **alst);
+
+# endif
 
 #endif
