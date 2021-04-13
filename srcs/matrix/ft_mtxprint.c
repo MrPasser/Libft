84645915f0b8038bb2b6f5a1b2bbc692fa9831d3
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2021-04-13 16:28:53 -0700

    fix lines when print matrix

--- srcs/matrix/ft_mtxprint.c
+++ srcs/matrix/ft_mtxprint.c
@@ -6,7 +6,7 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 10:03:07 by svet              #+#    #+#             */
-/*   Updated: 2021/04/13 16:19:43 by svet             ###   ########.fr       */
+/*   Updated: 2021/04/13 16:28:29 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ void	ft_mtxprint(t_matrix mtx)
 	size_t j;
 
 	i = 0;
-	j = 0;
 	while (i < mtx.rows)
 	{
+		j = 0;
 		while (j < mtx.columns)
 		{
 			ft_printf("%lf", ft_mtxgetelem(mtx, i, j));
+			ft_putchar(j == mtx.columns -1 ? '\n': ' ');
 			++j;
 		}
-		ft_putchar(j == mtx.columns ? '\n': ' ');
 		++i;
 	}
 }
