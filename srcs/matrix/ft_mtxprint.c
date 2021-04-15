22e0b2363051bc4a27a5f4de0fcd4a34e0660602
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2021-04-14 21:18:19 -0700

    small adjustment for fdf

--- srcs/matrix/ft_mtxprint.c
+++ srcs/matrix/ft_mtxprint.c
@@ -6,7 +6,7 @@
 /*   By: svet <svet@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 10:03:07 by svet              #+#    #+#             */
-/*   Updated: 2021/04/13 18:33:58 by svet             ###   ########.fr       */
+/*   Updated: 2021/04/14 21:17:33 by svet             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	ft_mtxprint(t_matrix mtx)
 		j = 0;
 		while (j < mtx.columns)
 		{
-			printf("%2.0Lf%c", ft_mtxgetelem(mtx, i, j), j == mtx.columns - 1 ? '\n': ' ');
+			printf("%3.0Lf%c", ft_mtxgetelem(mtx, i, j), j == mtx.columns - 1 ? '\n': ' ');
 			++j;
 		}
 		++i;
